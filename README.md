17bcac601664a171b7165fa401f18bf891b7f31c
StefanoGalandrini <stefe68@gmail.com>
2023-04-21 19:43:15 +0200

    Fix visualization problems - explained in Readme

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # js-campominato-grid
 
-# Everything seems to work
+# There was a problem when, with some widths and number of cells in a row, you could see small empty spaces between the cells and the border of the container.
 
-#
+# To prevent that, the base dimension of the container is defined in javascript and can be modified.
+
+# Also, the width and height of cells is rounded to an integer. Then is multiplied for the number of cells in a row and applied as an inline style to the container.
+
+# Unfortunately I couldn't find a better solution...
--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,22 @@
-/*
-L'utente clicca su un bottone che genererà
-una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l'utente clicca su ogni cella,
-la cella cliccata si colora di azzurro
-ed emetto un messaggio in console
-con il numero della cella cliccata.
-
-Bonus
-Aggiungere una select accanto al bottone di generazione
-che fornisca una scelta tra tre diversi livelli di difficoltà:
-- 	con difficoltà 1 => 100 caselle,
-	con un numero compreso tra 1 e 100,
-	divise in 10 caselle per 10 righe;
--	con difficoltà 2 => 81 caselle,
-	con un numero compreso tra 1 e 81,
-	divise in 9 caselle per 9 righe;
--	con difficoltà 3 => 49 caselle,
-	con un numero compreso tra 1 e 49,
-	divise in 7 caselle per 7 righe;
-*/
-
 // declare global DOM variables
 const playBtn = document.querySelector("#play");
 const grid = document.querySelector(".grid");
+
+// defines the approx centainer width and height in pixels
+const baseContainerDim = 600;
 
 // add event listener to play button
 // all code will be inside that
 playBtn.addEventListener("click", function () {
 	grid.innerHTML = "";
+
 	const difficultyLevel = document.querySelector("#difficulty").value;
 
 	// build grid creating html elements
 	gridDimension(difficultyLevel);
 	let cellCount = totalCells;
-	let cellSize = Math.sqrt(totalCells);
-	const dimCell = cellDimension(cellSize);
+	let cellRow = Math.sqrt(totalCells);
+	const dimCell = cellDimension(cellRow);
 
 	for (let i = 1; i <= cellCount; i++) {
 		const cell = document.createElement("div");
@@ -52,6 +32,14 @@ playBtn.addEventListener("click", function () {
 
 		grid.appendChild(cell);
 	}
+	/*	dimensioning container to an integer
+		plus twice the border
+		to prevent small empty spaces between cells and container
+		with some widths and number of cells */
+	dimContainer = dimCell * cellRow + 8;
+	grid.style.opacity = 1;
+	grid.style.maxWidth = `${dimContainer}px`;
+	grid.style.maxHeight = `${dimContainer}px`;
 });
 
 function gridDimension(level) {
@@ -73,6 +61,8 @@ function gridDimension(level) {
 }
 
 function cellDimension(dim) {
-	const dimCell = (640 - 2 * 4) / dim;
+	//value rounded to integer to prevent
+	// small empty spaces between cells and container
+	const dimCell = Math.round((baseContainerDim - 2 * 4) / dim);
 	return dimCell;
 }
